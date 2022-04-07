ca1ab8fb0a8019a258617b23d9325956524afb18
Felipe Navas <felipefnavas@gmail.com>
2022-04-07 00:38:40 -0300

    feat: additing regex to get urls

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 import chalk from "chalk";
 import fs from "fs";
 
+function extractLinks(text) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+
+  const resultArray = [];
+  let temp;
+  while ((temp = regex.exec(text)) !== null) {
+    resultArray.push({
+      [temp[1]]: temp[2],
+    });
+  }
+  return resultArray;
+}
+
 async function getFile(filepath) {
   const encoding = "utf-8";
   try {
     const text = await fs.promises.readFile(filepath, encoding);
-    console.log(chalk.blue(text));
+    console.log(extractLinks(text));
   } catch (err) {
     throw new Error(chalk.red(err.code, "não há arquivo no caminho"));
-  } finally {
-    console.log(chalk.yellow("operação finalizada!"));
   }
 }
 
